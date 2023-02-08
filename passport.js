bc6a17e17755ffb0a2061f6d41082755b4297082
DamStell <82368599+DamStell@users.noreply.github.com>
2023-02-08 07:47:27 +0100

    Add files via upload

---
+++ passport.js
@@ -0,0 +1,61 @@
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const GithubStrategy = require("passport-github2").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const passport = require("passport");
+
+const GOOGLE_CLIENT_ID =
+  "936122696140-rjh4ccs4gi76avlv05hu84uh64ejb7ok.apps.googleusercontent.com";
+const GOOGLE_CLIENT_SECRET = "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";
+
+GITHUB_CLIENT_ID = "your id";
+GITHUB_CLIENT_SECRET = "your id";
+
+FACEBOOK_APP_ID = "2027397574283434";
+FACEBOOK_APP_SECRET = "bc189d0710a2464111fdc32d64d8f4ac";
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new GithubStrategy(
+    {
+      clientID: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+      callbackURL: "/auth/github/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: FACEBOOK_APP_ID,
+      clientSecret: FACEBOOK_APP_SECRET,
+      callbackURL: "/auth/facebook/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
